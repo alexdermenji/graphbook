d5157560e9b3c92a8c1e44f640526b8fa8205bbc
Alexandr Dermenji <allexdermenji@gmail.com>
2022-11-05 17:02:54 +0000

    add first react component

--- src/client/App.js
+++ src/client/App.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialPosts = [
+  {
+    id: 2,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar1.png',
+      username: 'Test User',
+    },
+  },
+  {
+    id: 1,
+    text: 'Lorem ipsum',
+    user: {
+      avatar: '/uploads/avatar2.png',
+      username: 'Test User 2',
+    },
+  },
+];
 
 const App = () => {
+  const [posts, setPosts] = useState(initialPosts);
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="feed">
+        {initialPosts.map((post, i) => (
+          <div className="post" key={post.id}>
+            <div className="header">
+              <img src={post.user.avatar} alt="avatar" />
+              <h2> {post.user.username}</h2>
+            </div>
+            <p className="content">{post.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
